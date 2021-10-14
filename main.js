7d35ce7fce42a0d7da875fe6a52a22fdce404e13
Stephanie <stephaniereyes@pursuit.org>
2021-10-14 14:45:29 -0400

    more code

--- main.js
+++ main.js
@@ -17,14 +17,23 @@ fetch("https://ghibliapi.herokuapp.com/films")
   let movieTitle = document.querySelector("#movie-title");
   const movieYear = document.querySelector("#movie-released-date");
   const movieDescrip = document.querySelector("#movie-description");
-
+  
+  let selectedFilm;
 
 dropDown.addEventListener("change", ()=>{
     for (let film of films){
         if(dropDown.value === film.title){
             console.log(film);
+            selectedFilm = film;
+            movieTitle.textContent = film.title;
+            movieYear.textContent =  film.release_date;
+            movieDescrip.textContent = film.description;
+          
+
         }
     }
 })
+const addButton = document.querySelector("#save-button");
+const reviews = document.querySelector("#list_review");
     
 })
